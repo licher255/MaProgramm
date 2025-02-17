abe557b4213995c3641d718759674ea8bb6cae8d
licher <licher255@outlook.com>
2025-02-17 22:32:11 +0100

    update

---
+++ 00 RT_calculation.py
@@ -0,0 +1,29 @@
+"""
+Literature : Fundamentals of Ultrasonic Nondestructive Evaluation, Lester W.Schmerr Jr.
+
+This is a program which calculates the reflection and transmission coefficients.
+considering the situation of Cyro-UT, there are only 2 interfaces:
+1. a fluid-solid interface (Oblique incidence)
+2. a solid-solid Interface
+
+density : $rho$
+wave speed : $c$
+
+incident wave:
+reflected wave:
+transmitted wave:
+
+transmission coefficient:
+reflection coefficient:
+
+transmission coefficient of Ligitudinal wave
+transmission coefficient of transversal wave
+
+reflection coefficient of Ligitudinal wave
+reflection coefficient of transversal wave
+
+
+
+
+
+"""
---
+++ readme.md
@@ -0,0 +1,2 @@
+# UT-Calculation
+This is a project for UT-calculation. inclusive material data base and the following calculation.
